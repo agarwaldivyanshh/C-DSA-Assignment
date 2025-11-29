7361c52ee885abfa4e61340879c51d3c1b3c0bda
Divyansh Agarwal <agarwaldivyansh1706@gmail.com>
2025-11-29 23:26:30 +0530

    Assignments

---
+++ Assignment_1/Question_3.cpp
@@ -0,0 +1,15 @@
+/*3) Predict the Output of the following program*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i;
+    int arr[5] = {1};
+    for (i = 0; i < 5; i++)
+        printf("%d", arr[i]);
+    return 0;
+}
+
+// Output : 10000
+
